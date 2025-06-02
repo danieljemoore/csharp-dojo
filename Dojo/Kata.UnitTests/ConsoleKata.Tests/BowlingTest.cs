6b8736fdce8bfdc211a7d99552781d7e89275998
danieljemoore <contact@danieljemoore.com>
2025-06-01 21:21:10 -0500

    bowling, unfinished

---
+++ Dojo/Kata.UnitTests/ConsoleKata.Tests/BowlingTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Kata.ConsoleKata; // Assuming the Bowling class is in this namespace
+
+namespace Kata.UnitTests.ConsoleKata.Tests
+{
+    [TestFixture]
+    public class BowlingTest 
+    {
+        BowlingGame game;
+
+        public BowlingTest()
+        {
+            // Initialize any required resources or state here
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            // This method is called before each test runs
+            // You can use it to set up any common state or resources needed for the tests
+            game = new BowlingGame();
+        }
+
+        [Test]
+        public void Hookup()
+        {
+            Assert.That(true, Is.True, "This test is a placeholder to ensure the test framework is set up correctly.");
+        }
+
+        [Test]
+        public void GutterBalls()
+        {
+            ManyOpenFrames(10, 0, 0);
+            //AssertEquals(0, game.Score());
+            Assert.That(game.Score(), Is.EqualTo(0), "The score should be 0 for 10 frames of gutter balls.");
+        }
+
+        [Test]
+        public void Threes()
+        {
+            ManyOpenFrames(10, 3, 3);
+            //AssertEquals(60, game.Score());
+            Assert.That(game.Score(), Is.EqualTo(60), "The score should be 60 for 10 frames of three pins each.");
+        }
+        [Test]
+        public void Spare()
+        {
+            game.Spare(4, 6);
+            game.OpenFrame(3, 5);
+            ManyOpenFrames(8, 0, 0);
+            //AssertEquals(21, game.Score());
+            Assert.That(game.Score(), Is.EqualTo(21), "The score should be 21 for a spare followed by an open frame with 3 and 5.");
+        }
+
+        [Test]
+        public void Spare2()
+        {
+            game.Spare(4, 6);
+            game.OpenFrame(5, 3);
+            ManyOpenFrames(8, 0, 0);
+            Assert.That(game.Score(), Is.EqualTo(23), "The score should be 23 for a spare followed by an open frame with 5 and 3.");
+        }
+
+        private void ManyOpenFrames(int count, int firstThrow, int secondThrow)
+        {
+            for (int frameNumber = 0; frameNumber < count; frameNumber++)
+                game.OpenFrame(firstThrow, secondThrow);
+        }
+    }
+}
---
+++ Dojo/Kata/ConsoleKata/BowlingGame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.ConsoleKata
+{
+    public class BowlingGame
+    {
+        ArrayList throws;
+        ArrayList frames;
+
+        public BowlingGame()
+        {
+            // Initialize the game state here
+            frames = new ArrayList();
+            throws = new ArrayList();
+        }
+        public void OpenFrame(int firstThrow, int secondThrow)
+        {
+            throws.Add(firstThrow);
+            throws.Add(secondThrow);
+            frames.Add(new OpenFrame(firstThrow, secondThrow));
+        }
+        public void Spare(int firstThrow, int secondThrow)
+        {
+            frames.Add(new SpareFrame(firstThrow, secondThrow));
+        }
+        public int Score()
+        {
+            int total = 0;
+            foreach (IFrame frame in frames)
+                total += frame.Score();
+            return total;
+        }
+    }
+    public interface IFrame
+    {
+        int Score();
+    }
+    public class OpenFrame : IFrame
+    {
+        int score;
+
+        public OpenFrame(int firstThrow, int secondThrow)
+        {
+            score = firstThrow + secondThrow;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+    public class SpareFrame : IFrame
+    {
+        int score;
+
+        public SpareFrame(int firstBall, int secondBall)
+        {
+            score = 10 + NextBall();
+        }
+
+        private int NextBall()
+        {
+            return 3;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
